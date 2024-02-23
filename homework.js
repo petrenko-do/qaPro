c40dd0d1e92ea2bb747813cc1f3c108736a27682
Den <densamigos@ukr.net>
2024-02-23 20:59:48 +0100

    add homework3

--- homework.js
+++ homework.js
@@ -1,14 +1,36 @@
-let age = prompt('skilki rokiv?');
-if (!isNaN(age) && age > 0) {
-    if (age == 1) {
-        console.log(age + ' рiк')
-    } else if (age <= 4) {
-        console.log(age + ' роки')
-    } else {
-        console.log(age + ' рокiв')
-    }
-} else if (age == null) {
-    console.log('Действие отмененно')
-} else {
-    console.log('Введеное значение неверно')
-}
+// let star = "*";
+// let i = 1;
+// while (i <= 5){
+//     console.log(star.repeat(i));
+//     i++;
+// }
+
+// let str = '';
+// function star (a,b){
+//     for (let i = 0; i < a; i++){
+//        str += b;
+//        console.log(str);    }
+// }
+// star( 5, '*')
+
+
+// for (let i = 1; i <= 100; i++){
+//     if (i % 3 ===0) continue;
+//     console.log('Числа не кратные 3 ' + i);
+    
+// }
+
+
+// let num1 = prompt('Введи первое число'); 
+// let num2 = prompt('Введи второе число');
+// function alarm(a, b){
+//     alert('Квадрат твоих чисел = ' + Math.pow(a, b))
+// }
+// alarm(num1, num2)
+
+
+// function pow(a,b){
+//     return Math.pow(a, b)
+// }
+// let yourNumber = pow(2,3);
+// console.log(yourNumber);
