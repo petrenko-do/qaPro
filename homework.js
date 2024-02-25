1fb6b34dbe81d12ca96c95d8c372f689667cd1da
petrenko-do <60851781+petrenko-do@users.noreply.github.com>
2024-02-25 10:22:22 +0100

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,36 +1,36 @@
-// let star = "*";
-// let i = 1;
-// while (i <= 5){
-//     console.log(star.repeat(i));
-//     i++;
-// }
+let star = "*";
+let i = 1;
+while (i <= 5){
+    console.log(star.repeat(i));
+    i++;
+}
 
-// let str = '';
-// function star (a,b){
-//     for (let i = 0; i < a; i++){
-//        str += b;
-//        console.log(str);    }
-// }
-// star( 5, '*')
+let str = '';
+function star (a,b){
+    for (let i = 0; i < a; i++){
+       str += b;
+       console.log(str);    }
+}
+star( 5, '*')
 
 
-// for (let i = 1; i <= 100; i++){
-//     if (i % 3 ===0) continue;
-//     console.log('Числа не кратные 3 ' + i);
+for (let i = 1; i <= 100; i++){
+    if (i % 3 ===0) continue;
+    console.log('Числа не кратные 3 ' + i);
     
-// }
+}
 
 
-// let num1 = prompt('Введи первое число'); 
-// let num2 = prompt('Введи второе число');
-// function alarm(a, b){
-//     alert('Квадрат твоих чисел = ' + Math.pow(a, b))
-// }
-// alarm(num1, num2)
+let num1 = prompt('Введи первое число'); 
+let num2 = prompt('Введи второе число');
+function alarm(a, b){
+    alert('Квадрат твоих чисел = ' + Math.pow(a, b))
+}
+alarm(num1, num2)
 
 
-// function pow(a,b){
-//     return Math.pow(a, b)
-// }
-// let yourNumber = pow(2,3);
-// console.log(yourNumber);
+function pow(a,b){
+    return Math.pow(a, b)
+}
+let yourNumber = pow(2,3);
+console.log(yourNumber);
