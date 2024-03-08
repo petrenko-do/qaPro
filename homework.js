33766e2385a86d5b2c25f5a9cb0b85f3a395ddba
Den <densamigos@ukr.net>
2024-03-08 04:06:31 +0100

    hello

--- homework.js
+++ homework.js
@@ -1,31 +1,92 @@
-function checkProbabilityTheory(count) {
-    let pair_number = 0;
-    let odd_number = 0;
-  
-    for (let i = 0; i < count; i++) {
-        let rand = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
-
-        if (rand % 2 === 0) {
-            pair_number++
-        }else {
-            odd_number++
-        }
-    } 
-
-    console.log('Количество сгенерированных чисел: ' + (pair_number + odd_number))
-    console.log('Количество парных чисел: ' + pair_number);
-    console.log('Количество не парных чисел: ' + odd_number);
-    console.log('Процент парных к не парным: ' + ((pair_number/odd_number)*100).toFixed(2) + ' %');
-}
-checkProbabilityTheory(9999)
-
-
-function myIsNaN(value){
-    
-    if(typeof value !== 'number' ){
-        return true
+let obj = {
+    name: 'Huan',
+    age: 53,
+    sex: 'male',
+    isWork: true,
+    get addNewValue(){
+         this.name;
+    },
+    set addNewValue(value){
+        this.name = value
+    },
+    getInfo: function(){
+       if(this.hasOwnProperty('name' )) {
+        return console.log(this.name);
+       }
+       else {
+         this;
+       }
     }
-    
-    return value !== value
- 
 }
+
+obj.addNewValue = "Pedro";
+obj.getInfo();
+console.log(obj)
+
+
+
+
+
+
+var services = {
+
+    "стрижка": '60 грн',
+
+    "гоління": '80 грн',
+
+    "Миття голови": '100 грн',
+
+    newService: function (key, value) {
+        this[key] = value;
+
+        console.log(`Вы додали услугу "${key}" по цене "${value}"`);
+    },
+
+    sumPrice: function () {
+        let sum = 0;
+        for (let key in this) {
+            if (typeof this[key] === 'string') {
+                sum += parseInt(this[key]);
+            }
+        }
+        console.log("Общая сумма прайса: " + sum);
+
+    },
+    minPrice: function () {
+        let min = Infinity; 
+        for (let key in this) {
+            if (typeof this[key] === 'string') {
+                let price = parseInt(this[key]); 
+                if (!isNaN(price)) { 
+                    min = Math.min(min, price); 
+                }
+            }
+        }
+        console.log("Минимальная цена в прайсе: " + min);
+    },
+
+    maxPrice : function(){
+        let max = -Infinity;
+        for(let key in this){
+             if ( typeof this[key] === 'string'){
+                let price = parseInt(this[key]);
+                if (!isNaN(price)) {
+                    max = Math.max(max, price)
+                }
+             }
+        }
+        console.log("Максимальная цена в прайсе: " + max)
+    }
+
+};
+
+
+services.newService('Рассказать стих', '20 грн');
+services.newService('Поговорить о космосе', '80 грн');
+services['Розбити скло'] = "10 грн";
+
+
+services.sumPrice();
+services.maxPrice();
+services.minPrice();
+console.log(services);
